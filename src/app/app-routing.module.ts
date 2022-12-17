a159ea411c178fc451cce392685d13049cebe9f0
sk8rboil <sk8rboil008@windowslive.com>
2022-12-17 14:02:08 +0700

    add cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { MovieListComponent } from './component/movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'movielist',pathMatch: 'full'},
+  {path: 'movielist', component: MovieListComponent},
+  {path: 'cart', component: CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
